75e1018c233bab1e73da083dd7198d9e9feea59d
Davonium Technologies <dawudaminu4321@gmail.com>
2026-08-13 11:57:00 +0100

    Add APP_CONSTANTS to constants.js

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+export const APP_CONSTANTS = {
+    APP_NAME: 'SumNova V2',
+    COMPANY: 'Davonium Technologies',
+    VERSION: '2.0.0',
+    SUPPORTED_SUMMARY_TYPES: ['quick', 'detailed', 'keypoints', 'studynotes']
+};
