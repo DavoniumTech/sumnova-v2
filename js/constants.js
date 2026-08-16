28ab96f862f3286282f5878aa258747389197532
Davonium Technologies <dawudaminu4321@gmail.com>
2026-08-16 06:24:57 +0100

    Split APP_CONSTANTS into separate constants
    Refactor APP_CONSTANTS into individual exports for clarity.

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,3 @@
-export const APP_CONSTANTS = {
-    APP_NAME: 'SumNova V2',
-    COMPANY: 'Davonium Technologies',
-    VERSION: '2.0.0',
-    SUPPORTED_SUMMARY_TYPES: ['quick', 'detailed', 'keypoints', 'studynotes']
-};
+export const APP_NAME = 'SumNova V2';
+export const COMPANY_NAME = 'Davonium Technologies';
+export const BRAND_PRIMARY = '#6D28D9';
